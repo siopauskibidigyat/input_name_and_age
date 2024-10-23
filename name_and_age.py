5cff3b7714869f0f5f4f7252ee36665409506098
unknown <klarenz0620@gmail.com>
2024-10-23 19:25:15 +0800

    print name_and_age, and slight changes sa pseudo code

--- name_and_age.py
+++ name_and_age.py
@@ -6,14 +6,22 @@ while True:
         try:
             name = input("Please enter your name: ")
             age = int(input("Please enter your age: "))
+#input will be stored in array           
+            name_and_age = {
+                "name" : name,
+                "age" : age
+            }
+
+            print(name_and_age)
         except:
             print("ENGKKKK!!")
     #if age <= than 116(the age of the oldest person alive as of now)
         #if age > 116 then print "are you sure that is your true age? yes or no"    
     #if the user input a letter in age
         #print("Please input a valid number")
+    #if name has numbers
+        #print "Name must not contain numbers"
 #if no error, ask if user want to continue: yes\no
     #if yes, ask again for input
-    #if no, break loop
-        #print the name and age of the oldest person the user has input
+    #if no, print the name and age of the oldest person the user has input
 
