c8082738c812cb0fd0a52216391a61b0cad5f67e
unknown <klarenz0620@gmail.com>
2024-10-23 21:59:09 +0800

    gumagana pero may kulang, big changes sa pseudo code

--- name_and_age.py
+++ name_and_age.py
@@ -1,26 +1,34 @@
 #identify the array  as name_and_age
 name_and_age = {}
-#ask user to input the name and age
+#ask user to input the name_and_age
+#loop 1: input number
 while True:
+    #loop2: retry when input raised an error 
     while True:
         try:
+            #if name has numbers
+                #print "Name must not contain numbers"
             name = input("Please enter your name: ")
-            age = int(input("Please enter your age: "))
+            if not name.isalpha():
+                    print("Error: Name must not contain numbers.")
+                    break
+                #age should be <= than 116(the age of the oldest person alive as of now)
+                    #if age > 116 then print "no one is that old"
+            while True:
+                    age = int(input("Please enter your age: "))
+                    if age > 116:
+                        print("no person alive is that old")
+                        continue
 #input will be stored in array           
-            name_and_age = {
-                "name" : name,
-                "age" : age
-            }
+                    name_and_age = {
+                        "name" : name,
+                        "age" : age
+                    }
 
-            print(name_and_age)
+                    print(name_and_age)
+                    break
         except:
-            print("ENGKKKK!!")
-    #if age <= than 116(the age of the oldest person alive as of now)
-        #if age > 116 then print "are you sure that is your true age? yes or no"    
-    #if the user input a letter in age
-        #print("Please input a valid number")
-    #if name has numbers
-        #print "Name must not contain numbers"
+            print("ENGKKKK!!")      
 #if no error, ask if user want to continue: yes\no
     #if yes, ask again for input
     #if no, print the name and age of the oldest person the user has input
