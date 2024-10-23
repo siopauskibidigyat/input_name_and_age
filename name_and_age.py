43224bdf80ac4d5637dad3aa116d5184936fced5
unknown <klarenz0620@gmail.com>
2024-10-23 18:53:24 +0800

    pseudo code

---
+++ name_and_age.py
@@ -0,0 +1,14 @@
+#identify the array  as name_and_age
+name_and_age = {}
+
+#ask user to input the name and age
+    #if age <= than 116(the age of the oldest person alive as of now)
+        #if age > 116 then print "no one has ever lived that long, dumbass"    
+    #if the user input a letter in age
+        #print("Please input a valid number")
+#the input will be stored in array
+#if no error, ask if user want to continue: yes\no
+    #if yes, ask again for input
+    #if no, break loop
+        #print the name and age of the oldest person the user has input
+
