dedaaef9bfa01457a73543ad3d46643dace52699
unknown <klarenz0620@gmail.com>
2024-10-23 19:09:40 +0800

    input name and age

--- name_and_age.py
+++ name_and_age.py
@@ -1,12 +1,17 @@
 #identify the array  as name_and_age
 name_and_age = {}
-
 #ask user to input the name and age
+while True:
+    while True:
+        try:
+            name = input("Please enter your name: ")
+            age = int(input("Please enter your age: "))
+        except:
+            print("ENGKKKK!!")
     #if age <= than 116(the age of the oldest person alive as of now)
-        #if age > 116 then print "no one has ever lived that long, dumbass"    
+        #if age > 116 then print "are you sure that is your true age? yes or no"    
     #if the user input a letter in age
         #print("Please input a valid number")
-#the input will be stored in array
 #if no error, ask if user want to continue: yes\no
     #if yes, ask again for input
     #if no, break loop
